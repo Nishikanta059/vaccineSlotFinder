d71c033c94597ea96e1287aade8bb70a7b19b804
nishi059 <nishikantaparida059@gmail.com>
2021-05-06 12:48:24 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Center(child: Text("Find vaccine slots")),
+        title: Text("Find vaccine slots"),
         backgroundColor: Colors.purple[100],
         actions: [
           TextButton.icon(
